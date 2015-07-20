a7e79abf1616a75268e623a808c87fe75bd80ce2
Kamron Batman <kamron.batman@gmail.com>
2015-07-20 14:42:44 -0700

    Added a full solution.  Total of 174 characters minified.

--- islandscount.js
+++ islandscount.js
@@ -1,9 +1,27 @@
-function countIslands(m) {
+//This function passes all Makersquare tests.
+function m(m) {
   for ( i = 0, s = 0, p = m.indexOf('\n'); i < m.length; i++ ) {
     s += m[i] == '0' &&
       m[i-1] != '0' &&
       m[i-p-1] != '0' &&
       (m[i+1] != '0' || m[i-p] != '0')
+  }
+
+  return s;
+}
+
+//This is the proper function that will pass all test cases
+function countIslands (m) {
+  var r = function(idx,start) {
+    if (m[idx] === '0') {
+      m[idx] = (s += start);
+
+      r(idx-1,0), r(idx+p+1,0), r(idx+1,0), r(idx-p-1,0)
+    }
+  }
+
+  for ( var i = 0, s = 0, p = m.indexOf('\n'), m = m.split(''); i < m.length; i++ ) {
+    r(i,1);
   }
 
   return s;
